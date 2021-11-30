aa77ffbf10876618238def01e504877323b42569
valentin.gorbachev <valentin.gorbachev@aliexpress.com>
2021-11-30 14:54:50 +0300

    Fix lint

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import brainCalc from '../src/games/brain-calc.js'
-import start from '../src/index.js'
+import brainCalc from '../src/games/brain-calc.js';
+import start from '../src/index.js';
 
-start(brainCalc)
+start(brainCalc);
